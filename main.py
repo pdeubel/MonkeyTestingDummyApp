d3fa81761b9aacd38738f0582c89de69c06e5c2d
ildeniz-s <sekban.ildeniz@gmail.com>
2021-02-11 00:06:01 +0100

    Resize pygame

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from src.Application2 import Application2
 import pygame
 import numpy as np
 import time
+import cv2
 
 env = Application2()
+DISPLAY_SIZE = (1280, 720)
+ENV_SIZE = (env.width, env.height)
+WIDTH_RATIO = DISPLAY_SIZE[0] / ENV_SIZE[0]
+HEIGHT_RATIO = DISPLAY_SIZE[1] / ENV_SIZE[1]
 
 pygame.init()
-display = pygame.display.set_mode((env.width, env.height))
-pygame.surfarray.blit_array(display, env.frame_buffer)
+display = pygame.display.set_mode(DISPLAY_SIZE)
+resized = cv2.resize(env.frame_buffer, dsize=(DISPLAY_SIZE[1], DISPLAY_SIZE[0]))
+pygame.surfarray.blit_array(display, resized)
 pygame.display.update()
 
 running = True
@@ -18,7 +24,7 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             # Left click
             if event.button == 1:
-                point = np.array([event.pos[0], event.pos[1]])
+                point = np.array([event.pos[0] / WIDTH_RATIO, event.pos[1] / HEIGHT_RATIO])
                 start = time.process_time()
                 observation, reward, done = env.step(point)
                 print('Frame Time: ' + str((time.process_time() - start) * 1000) + ' ms')
@@ -26,12 +32,14 @@ while running:
                 print('Done: ' + str(done))
                 if done:
                     print(env.get_progress())
-                pygame.surfarray.blit_array(display, observation)
+                resized = cv2.resize(observation, dsize=(DISPLAY_SIZE[1], DISPLAY_SIZE[0]))
+                pygame.surfarray.blit_array(display, resized)
                 pygame.display.update()
             # Right click
             elif event.button == 3:
                 observation = env.reset()
-                pygame.surfarray.blit_array(display, observation)
+                resized = cv2.resize(observation, dsize=(DISPLAY_SIZE[1], DISPLAY_SIZE[0]))
+                pygame.surfarray.blit_array(display, resized)
                 pygame.display.update()
 
 
